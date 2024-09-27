ee0f8103e33b8c8ac9af9eae19790af789b71f9d
yaskour <y.askour@navimarsystems.com>
2024-09-27 13:45:00 +0100

    server is ready

---
+++ server/tcp/tcp.go
@@ -0,0 +1,40 @@
+package tcp
+
+import (
+	"fmt"
+	"net"
+)
+
+var count uint64 = 0
+
+func Listener() {
+	Listener, error := net.Listen("tcp", ":8080")
+	defer Listener.Close()
+	if error != nil {
+		fmt.Println("creating a listener failed")
+	}
+	for true {
+		connection, error := Listener.Accept()
+		count++
+		if error != nil {
+			fmt.Println("accepting a connection failed")
+		}
+		go handleConnection(&connection)
+	}
+}
+
+func handleConnection(connection *net.Conn) {
+	defer (*connection).Close()
+	buf := make([]byte, 1024)
+
+	for {
+		len, err := (*connection).Read(buf)
+		if err != nil {
+			break
+		}
+
+		message := string(buf[:len])
+
+		fmt.Printf(message)
+	}
+}
